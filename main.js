36740e06971747fbe101c9f26a2e06ad3759b8cc
az1mov1c <azimovmirabror20@gmail.com>
2023-07-22 15:31:01 +0300

    masala

---
+++ main.js
@@ -0,0 +1,23 @@
+// let a = 10
+// let b = 5 
+
+// console.log(a + b);
+
+
+// let a = 25 
+// let b = 15
+
+// let result = a - b 
+// console.log(result);
+
+// let  a = 10 
+// let  b = 5 
+
+// let c = a - b
+// console.log(c);
+
+// let a = 10
+// let b = 3
+// console.log(a/b);
+
+// console.log(2**10);
